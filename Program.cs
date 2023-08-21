ae5090ff49d08381d5a2e72fcf1d66570b06488f
João Oliveira <jpopereira2005@gmail.com>
2023-08-21 14:46:36 +0000

    estrutura if

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//condicional
+var flag = 50;
+
+if (flag>50) {
+        Console.WriteLine("maior");
+    }    
+    else {
+        Console.WriteLine("não é maior");
+    }
+
+Switch (flag); {case 50;:
+        Console.WriteLine("valor igual 50");
+    break;}  
+    
